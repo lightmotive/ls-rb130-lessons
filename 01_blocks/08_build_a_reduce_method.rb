be8a7ed063be7b2c9cb61e11813bb0953d4ac716
Andre Light <andrelight.al@gmail.com>
2022-07-25 01:12:06 +0000

    Implement `reduce` method
    - Match **Array#reduce** default param behavior.

--- 01_blocks/08_build_a_reduce_method.rb
+++ 01_blocks/08_build_a_reduce_method.rb
@@ -2,8 +2,25 @@
 
 # Write method...
 
+def reduce(array, start_value = nil)
+  acc = start_value.nil? ? array.first : start_value
+  array = array[1..] if start_value.nil?
+
+  array.each do |element|
+    acc = yield(acc, element)
+  end
+
+  acc
+end
+
 array = [1, 2, 3, 4, 5]
 
-reduce(array) { |acc, num| acc + num }                    # => 15
-reduce(array, 10) { |acc, num| acc + num }                # => 25
-reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p(reduce(array) { |acc, num| acc + num } == 15)
+p(reduce(array, 10) { |acc, num| acc + num } == 25)
+
+begin
+  reduce(array) { |acc, num| acc + num if num.odd? }
+  p false
+rescue NoMethodError
+  p true
+end
