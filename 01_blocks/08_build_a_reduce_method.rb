9d178218a397d2ec241bdf3b24544c92f4d85b0e
Andre Light <andrelight.al@gmail.com>
2022-07-25 01:02:41 +0000

    Set up `reduce` method lesson

---
+++ 01_blocks/08_build_a_reduce_method.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Write method...
+
+array = [1, 2, 3, 4, 5]
+
+reduce(array) { |acc, num| acc + num }                    # => 15
+reduce(array, 10) { |acc, num| acc + num }                # => 25
+reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
