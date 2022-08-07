2a618e9030d2a28fb689cf89628a2d81e47da07c
Andre Light <andrelight.al@gmail.com>
2022-08-07 21:29:29 +0000

    Use preferred spec-test syntax, `expect(...)`

--- 02_intro_to_testing/03_lecture_minitest/car_spec.rb
+++ 02_intro_to_testing/03_lecture_minitest/car_spec.rb
@@ -8,12 +8,7 @@ describe Car do
   describe 'when asked for wheels' do
     it 'has 4 wheels' do
       car = Car.new
-      _(car.wheels).must_equal 4
-    end
-
-    it '(fails with 3 wheels)' do
-      car = Car.new
-      _(car.wheels).must_equal 3
+      expect(car.wheels).must_equal 4
     end
   end
 end
