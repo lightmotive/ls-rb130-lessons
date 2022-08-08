c4819e2ccf2878aa74fff02c64b7d917ff1ba01b
Andre Light <andrelight.al@gmail.com>
2022-08-08 03:20:58 +0000

    Refactor test setup to separate class
    - Enable reuse for upcoming `TodoListSubsetTest`.

--- 02_intro_to_testing/03_lecture_minitest/todo_list_test.rb
+++ 02_intro_to_testing/03_lecture_minitest/todo_list_test.rb
@@ -4,22 +4,9 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 MiniTest::Reporters.use!
 
-require_relative 'todo_list'
+require_relative 'todo_list_test_core'
 
-class TodoListTest < MiniTest::Test
-  attr_reader :list, :list_default_title, :todos, :list_default_size
-
-  def setup
-    todo1 = Todo.new('Buy milk')
-    todo2 = Todo.new('Clean room')
-    todo3 = Todo.new('Go to gym')
-    @todos = [todo1, todo2, todo3]
-    @list_default_size = todos.size
-
-    @list_default_title = "Test Today's Todos"
-    @list = TodoList.new(@list_default_title, todos)
-  end
-
+class TodoListTest < TodoListTestCore
   def test_add
     add_returned = list.add(todos[0])
     assert_equal(list_default_size + 1, list.size)
---
+++ 02_intro_to_testing/03_lecture_minitest/todo_list_test_core.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'todo_list'
+
+class TodoListTestCore < MiniTest::Test
+  attr_reader :list, :list_default_title, :todos, :list_default_size
+
+  def setup
+    todo1 = Todo.new('Buy milk')
+    todo2 = Todo.new('Clean room')
+    todo3 = Todo.new('Go to gym')
+    @todos = [todo1, todo2, todo3]
+    @list_default_size = todos.size
+
+    @list_default_title = "Test Today's Todos"
+    @list = TodoList.new(@list_default_title, todos)
+  end
+end
