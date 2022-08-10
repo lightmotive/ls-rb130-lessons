91db9787ef165b59f24defd3ae7c8968de244a0f
Andre Light <andrelight.al@gmail.com>
2022-08-10 00:54:55 +0000

    Update comment re: rake task
    - Learn later if LS doesn't teach it.

--- 02_intro_to_testing/03_lecture_minitest/todo_app/todo_test_suite.rb
+++ 02_intro_to_testing/03_lecture_minitest/todo_app/todo_test_suite.rb
@@ -7,7 +7,13 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 MiniTest::Reporters.use!
 
-# Create a rake task to automate tests
+# Create a rake task to automate tests - LS will probably teach this in the
+# next lesson. If not, delve into these resources:
+# https://gist.github.com/jcasimir/3c687bd5db16a73b4cf3
+# http://docs.seattlerb.org/minitest/Minitest/TestTask.html
+# https://github.com/ruby/rake
+# https://backend.turing.edu/module1/lessons/project_etiquette#:~:text=with%20the%20flow.-,rakefiles,-Rake%20tasks%20come
+# https://www.stuartellis.name/articles/rake/
 
 require './test/todo_test'
 require './test/todo_list_test'
